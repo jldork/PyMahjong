24bbbdfa2d6dbb7983a215a547ae67d4f3f4d4b9
James Leung <jleung@stashinvest.com>
2022-01-15 11:35:53 -0600

    Mahjong Tiles
    - HK Mahjong Tiles
    - DRAGON, WIND, SEASON, FLOWER included

---
+++ test_tile.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+from tile import Tile, TILES
+
+class TileTest(TestCase):
+    def test_tile_counts_are_correct(self):
+        assert len(TILES) == 144
+
+        for suit in ['BAMBOO', 'CHARACTER', 'DOT']:
+            assert len([
+                tile for tile in TILES if tile.suit == suit
+            ]) == 36
+
+        assert len([
+            tile for tile in TILES if tile.suit == 'DRAGON'
+        ]) == 12
+
+        assert len([
+            tile for tile in TILES if tile.suit == 'WIND'
+        ]) == 16
+
+
---
+++ tile.py
@@ -0,0 +1,34 @@
+class Tile:
+    def __init__(self, suit:str, number:int):
+        self.suit = suit
+        self.number = number
+
+    def __repr__(self) -> str:
+        print("I AM RUNNING")
+        mapping = {
+            'SEASON': ['SPRING', 'SUMMER', 'AUTUMN', 'WINTER'],
+            'FLOWER': ['PLUM', 'LILY', 'CHRYSANTHEMUM', 'BAMBOO'],
+            'WIND': ['EAST', 'SOUTH', 'WEST', 'NORTH'],
+            'DRAGON': ['GREEN', 'RED', 'BLUE']
+        }
+        if self.suit in ['BAMBOO', 'DOT', 'CHARACTER']:
+            return f'{self.suit}: {self.number}'
+        else:
+            return f'{mapping.get(self.suit)[self.number - 1]} {self.suit}'
+
+
+TILES = 4*([
+    Tile(suit=suit, number=number) 
+    for suit in ['BAMBOO', 'DOT', 'CHARACTER']
+    for number in range(1,10)
+] + [
+    Tile(suit='WIND', number=number)
+    for number in range(1,5)
+] + [
+    Tile(suit='DRAGON', number=number)
+    for number in range(1,4)
+]) + [
+    Tile(suit=suit, number=number)
+    for suit in ['SEASON', 'FLOWER']   
+    for number in range(1,5)
+]
